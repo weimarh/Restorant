586263416640ee536c694a01180d64744e8521e1
weimarh <ggmwba@gmail.com>
2024-11-28 09:49:58 -0400

    minor changes

---
+++ DinnerRestorant/BuberDinner/BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,46 @@
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : 
+        IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : IErrorOr
+{    
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validationResult =  await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors
+            .Select(error => Error.Validation(
+                error.PropertyName,
+                error.ErrorMessage))
+            .ToList();
+
+        return (dynamic)errors;
+    }  
+}
